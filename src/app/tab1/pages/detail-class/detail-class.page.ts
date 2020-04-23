27552e1a05fb066160e036e11509a6430680a7a3
MMDJK <38125463+MMDJK@users.noreply.github.com>
2020-04-23 23:11:13 +0800

    添加班课详情页

--- src/app/tab1/pages/detail-class/detail-class.page.ts
+++ src/app/tab1/pages/detail-class/detail-class.page.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
+import { Component, OnInit, OnChanges, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-detail-class',
   templateUrl: './detail-class.page.html',
   styleUrls: ['./detail-class.page.scss'],
 })
-export class DetailClassPage implements OnInit {
+export class DetailClassPage implements OnInit, OnChanges, AfterViewInit {
 
-  constructor() { }
+  private role: any;
+  constructor(public localService: LocalStorageService) {
+   }
 
   ngOnInit() {
+
+  }
+
+  ngOnChanges() {
+    this.role = this.localService.getItem('user').role;
+    console.log(this.role);
+  }
+
+  ngAfterViewInit() {
+    
   }
 
 }
